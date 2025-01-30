f70b5767f93e7ca93a0d36338fe9becf97f5500b
AnthonyFinney <anthonyrockfinnie0@gmail.com>
2025-01-30 20:00:09 +0600

    commit

--- Data/ApplicationDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace Task5.Data;
-
-public class ApplicationDbContext : DbContext {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-}
--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(opts => {
-    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 var app = builder.Build();
 
